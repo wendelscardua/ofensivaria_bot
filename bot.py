e12fc6597ac8c4071f463338a484fd08c3df6e20
Fernando Takai <fernando.takai@gmail.com>
2015-06-24 19:07:33 -0300

    Moved commands to its own module

--- bot.py
+++ bot.py
@@ -1,80 +1,13 @@
-import re
 import redis
-import random
 import requests
 import logging
+import commands
 
 logger = logging.getLogger()
 
-class Command(object):
-
-    SLASH_COMMAND = None
-
-    def __init__(self, bot, redis):
-        self._bot = bot
-        self._redis = redis
-
-    def can_respond(self, message):
-        if self.SLASH_COMMAND:
-            return message['text'].startswith(self.SLASH_COMMAND)
-
-        return False
-
-    def process(self, bot, message):
-        if self.can_respond(message):
-            self.respond(message)
-
-class Ping(Command):
-    def can_respond(self, message):
-        return message['text'] == 'ping'
-
-    def respond(self, message):
-        self._bot.send_message(message['chat']['id'], 'pong')
-
-class Title(Command):
-    def can_respond(self, message):
-        return message['text'] == u'/title'
-    
-    def respond(self, message):
-        self._bot.send_message(message['chat']['id'], 'http://imgur.com/a/0OlQR')
-
-class EitherOr(Command):
-    regex = re.compile("([\s\w]+)\sou\s([\s\w]+)\??", re.UNICODE)
-
-    def can_respond(self, message):
-        text = message['text']
-        return '@ofensivaria_bot' in text and bool(self.regex.search(message['text']))
-
-    def respond(self, message):
-        text = re.split('\s|\xa0', message['text'], 1)[-1]
-        choices = self.regex.findall(text)[0]
-
-        if random.randint(1, 100) < 10:
-            answer = 'sim'
-        else:
-            answer = random.choice(choices)
-
-        self._bot.send_message(message['chat']['id'], answer, message.get('message_id', None))
-
-class PowerOff(Command):
-
-    SLASH_COMMAND = '/staph'
-
-    def respond(self, message):
-        self._bot.stop()
-
-class Help(Command):
-
-    SLASH_COMMAND = '/help'
-
-    def respond(self, message):
-        answer = 'Deus ajuda quem cedo madruga'
-        self._bot.send_message(message['chat']['id'], answer, message.get('message_id', None))
-
 class TelegramBot(object):
 
-    COMMANDS = [PowerOff, Ping, Title,
-                EitherOr, Help]
+    COMMANDS = commands.COMMANDS
 
     def __init__(self, token):
         self.__running = True
--- commands.py
+++ commands.py
@@ -1,20 +1,79 @@
+import re
+import random
+
 class Command(object):
+
+    SLASH_COMMAND = None
+    REPLY_NEEDED = False
+    REGEX = None
+
+    def __init__(self, bot, redis):
+        self._bot = bot
+        self._redis = redis
+
+    def can_respond(self, message):
+        if self.SLASH_COMMAND:
+            return message['text'].startswith(self.SLASH_COMMAND)
+
+        return False
+
     def process(self, bot, message):
+        text = message['text']
+
+        if self.REPLY_NEEDED and not text.startswith('@ofensivaria_bot'):
+            return
+
+        if self.REGEX and not self.REGEX.search(message['text']):
+            return
+
         if self.can_respond(message):
-            self.respond(bot, message)
+            self.respond(message)
 
 class Ping(Command):
     def can_respond(self, message):
-        return message == 'ping'
+        return message['text'] == 'ping'
 
-    def respond(self, bot, message):
-        print self.send_message(message['chat']['id'], 'ping')
+    def respond(self, message):
+        self._bot.send_message(message['chat']['id'], 'pong')
 
-class BotCommands(object):
-    COMMANDS = [Ping]
+class Title(Command):
+    def can_respond(self, message):
+        return message['text'] == u'/title'
     
-    def __init__(self):
-        self.__commands = [cls() for cls in self.COMMANDS]
+    def respond(self, message):
+        self._bot.send_message(message['chat']['id'], 'http://imgur.com/a/0OlQR')
 
-    def register(cls, command):
-        self.COMMANDS.append(command)
+class EitherOr(Command):
+    REPLY_NEEDED = True
+    REGEX = re.compile("([\s\w]+)\sou\s([\s\w]+)\??", re.UNICODE)
+
+    def can_respond(self, message):
+        return True
+
+    def respond(self, message):
+        text = re.split('\s|\xa0', message['text'], 1)[-1]
+        choices = self.regex.findall(text)[0]
+
+        if random.randint(1, 100) < 10:
+            answer = 'sim'
+        else:
+            answer = random.choice(choices)
+
+        self._bot.send_message(message['chat']['id'], answer, message.get('message_id', None))
+
+class PowerOff(Command):
+
+    SLASH_COMMAND = '/staph'
+
+    def respond(self, message):
+        self._bot.stop()
+
+class Help(Command):
+
+    SLASH_COMMAND = '/help'
+
+    def respond(self, message):
+        answer = 'Deus ajuda quem cedo madruga'
+        self._bot.send_message(message['chat']['id'], answer, message.get('message_id', None))
+
+COMMANDS = [PowerOff, Ping, Title, EitherOr, Help]
