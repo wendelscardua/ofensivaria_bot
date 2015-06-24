d299e93d61a5c6e290d0d1c2c73f655e7b3409c8
Fernando Takai <fernando.takai@gmail.com>
2015-06-24 18:59:35 -0300

    Added initial flask support for webhooks

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
+import sys
+import logging
+
 from flask import Flask
+from flask import request
+
+from bot import TelegramBot
 
 app = Flask(__name__)
+app.config.from_envvar("CONFIG_FILE")
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+bot = TelegramBot(app.config['TOKEN'])
+
+logger.propagate = False
 
 app.route('/bot-webhook')
 def webhook():
-    print 'webhookmemaybe'
+    update = request.get_json()
+    bot.process_update(update)
+    return dict(status='ok')
 
 if __name__ == '__main__':
+    me = bot.me()
+    logger.info("Bot named %(first_name)s/%(username)s" % me['result'])
+    app.logger.addHandler(handler)
     app.run(host="0.0.0.0")
